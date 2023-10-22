ee4d175baa76548c8a7614945bd08106a27b61bb
Riley Chang <tkngaejcpi@gmail.com>
2023-10-22 17:07:18 +0800

    add some comments in the flake of nodejs

--- nodejs/flake.nix
+++ nodejs/flake.nix
@@ -8,27 +8,36 @@
   outputs = {nixpkgs, ...}: let
     system = "x86_64-linux";
     pkgs = import nixpkgs {inherit system;};
+
+    nodejs = pkgs.nodejs; # specify node version here
   in {
     devShells."${system}".default = pkgs.mkShell {
-      buildInputs = with pkgs.nodePackages;
+      buildInputs =
         [
-          nodejs # change it to specific version if necessary
-
+          nodejs
+        ]
+        ++ (with pkgs.nodePackages; [
           npm
           yarn
           pnpm
-        ]
+        ])
         ++ (with pkgs; [
-          # use vscodium as ide, delete it if not necessary
+          # nix formatter
+          alejandra
+
+          # use vs-codium as ide
           (vscode-with-extensions.override {
             vscode = vscodium;
 
             vscodeExtensions = with vscode-extensions; [
+              # language pack
               ms-ceintl.vscode-language-pack-zh-hant
 
+              # for nix
               bbenoist.nix
               vscode-extensions.kamadorueda.alejandra
 
+              # for js
               esbenp.prettier-vscode
             ];
           })
